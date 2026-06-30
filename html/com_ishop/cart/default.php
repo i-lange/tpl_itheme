ebf250ecd70936dcc3359f3868281be9400b87e4
Pavel Lange <pavel@ilange.ru>
2026-06-30 15:07:50 +0300

    Новый дизайн корзины.

--- html/com_ishop/cart/default.php
+++ html/com_ishop/cart/default.php
@@ -46,12 +46,12 @@ if ($this->cart->total > 0) {
     $dataLayer = 'gtag("event","view_cart",{currency:"' . $currency . '",value:"' . $this->cart->summary . '",items:dataLayerItems});';
     $wa->addInlineScript($dataLayer);
 }
+$count = count($this->cart->products);
 ?>
-<div class="container pb-5">
-    <?php if ($this->params->get('show_page_heading')) : ?>
-        <h1 class="mb-1"><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
-    <?php endif; ?>
-    <?php if (count($this->cart->products) > 0) : ?>
+<?php if ($this->params->get('show_page_heading')) : ?>
+    <h1><?php echo $this->escape($this->params->get('page_heading')); ?> <span class="fw-normal text-body-tertiary"><?php echo $count; ?> шт.</span></h1>
+<?php endif; ?>
+<?php if ($count > 0) : ?>
     <form class="module-cart-grid"
           id="cart-submit"
           action="<?php echo Route::_(RouteHelper::getCheckoutRoute()); ?>"
@@ -61,10 +61,9 @@ if ($this->cart->total > 0) {
         <div>
             <?php foreach ($this->cart->products as $key_id => $product) : ?>
                 <div class="module-cart-item<?php echo ($product->available) ? '' : ' not_available'; ?>" data-product-incart-id="<?php echo $product->id; ?>">
-                    <div class="cart-item_top">
-                        <div class="cart-item_image">
-                            <?php echo LayoutHelper::render('itheme.image_product_small', $product); ?>
-                            <?php if ($product->available) : ?>
+                    <?php echo LayoutHelper::render('itheme.product.thumb', ['item' => $product, 'class' => 'cart-item__image']); ?>
+                    <?php if ($product->available) : ?>
+                        <div class="cart-item__checkbox">
                             <label class="visually-hidden" for="check_input_<?php echo $key_id; ?>"></label>
                             <input class="form-check-input"
                                    id="check_input_<?php echo $key_id; ?>"
@@ -72,66 +71,54 @@ if ($this->cart->total > 0) {
                                    name="products[]"
                                    checked="checked"
                                    value="<?php echo $product->id; ?>">
-                            <?php endif; ?>
                         </div>
-                        <div class="cart-item_title">
-                            <div class="title-container">
-                                <h3><?php echo htmlspecialchars($product->fullname); ?></h3>
-                                <span>
-                                <?php if ($product->available) : ?>
-                                    <?php echo Text::_('COM_ISHOP_TAB_DELIVERIES'), ' ', $product->delivery; ?>
-                                <?php else: ?>
-                                    <?php echo Text::_('COM_ISHOP_PRODUCT_NOT_AVAILABLE'); ?>
-                                <?php endif; ?>
-                                </span>
-                                <a class="cover" href="<?php echo Route::_(RouteHelper::getProductRoute((int)$product->id, (int)$product->catid)); ?>"></a>
-                            </div>
-                            <div class="prices-container">
-                                <?php if ($product->available) : ?>
-                                    <span class="cart-item_price<?php echo ($product->discount_size > 0) ? ' sale' : '';?>">
-                                    <?php if ($product->discount_size > 0) : ?>
-                                        <?php echo LayoutHelper::render('itheme.icon', ['icon' => 'i-sale']); ?>
-                                    <?php endif; ?>
-                                    <span data-price><?php echo PriceHelper::renderPrice($product->incart_total, false); ?></span><span class="currency"><?php echo $currency; ?></span>
-                                </span>
-                                    <?php if ($product->incart_old_total > 0) : ?>
-                                        <span class="cart-item_price_old_price">
-                                        <del data-old-price><?php echo PriceHelper::renderPrice($product->incart_old_total, false); ?><span class="currency visually-hidden"><?php echo $currency; ?></span></del>
-                                    </span>
-                                    <?php endif; ?>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="cart-item_controls">
-                        <div class="cart-item_quantity">
+                    <?php endif; ?>
+                    <div class="cart-item__info">
+                        <div class="position-relative">
                             <?php if ($product->available) : ?>
-                            <label class="visually-hidden" for="quantity_input_<?php echo $key_id; ?>"></label>
-                            <button class="btn btn-primary"
-                                    title="<?php echo Text::_('COM_ISHOP_CART_MINUS'); ?>"
-                                    type="button"
-                                    data-cart-button="minus"><span class="visually-hidden"><?php echo Text::_('COM_ISHOP_CART_MINUS'); ?></span>-</button>
-                            <input class="quantity_input"
-                                   id="quantity_input_<?php echo $key_id; ?>"
-                                   type="number"
-                                   name="quantity[<?php echo $product->id; ?>]"
-                                   value="<?php echo $product->incart_count; ?>"
-                                   min="0"
-                                   readonly
-                                   data-quantity>
-                            <button class="btn btn-primary"
-                                    title="<?php echo Text::_('COM_ISHOP_CART_PLUS'); ?>"
-                                    type="button"
-                                    data-cart-button="plus"><span class="visually-hidden"><?php echo Text::_('COM_ISHOP_CART_PLUS'); ?></span>+</button>
+                                <?php echo LayoutHelper::render('itheme.product.prices', ['item' => $product]); ?>
+                                <?php if (!empty($product->delivery)) : ?>
+                                    <div><span class="text-body-emphasis">Доставим</span> <?php echo $product->delivery; ?></div>
+                                <?php endif; ?>
+                            <?php else: ?>
+                                <?php echo Text::_('COM_ISHOP_PRODUCT_NOT_AVAILABLE'); ?>
                             <?php endif; ?>
+                            <h3><?php echo htmlspecialchars($product->fullname); ?></h3>
+                            <?php if ($product->introtext !== '') : ?>
+                                <p class="mt-2 d-n d-md-block text-body-emphasis"><?php echo $product->introtext; ?></p>
+                            <?php endif; ?>
+                            <a class="stretched-link" href="<?php echo Route::_(RouteHelper::getProductRoute((int)$product->id, (int)$product->catid)); ?>">
+                                <span class="visually-hidden"><?php echo $this->escape($product->fullname); ?></span>
+                            </a>
                         </div>
-                        <div class="cart-item_remove">
+                        <div class="cart-item_controls">
+                            <div class="cart-item_quantity">
+                                <?php if ($product->available) : ?>
+                                    <label class="visually-hidden" for="quantity_input_<?php echo $key_id; ?>"></label>
+                                    <button class="btn btn-primary"
+                                            title="<?php echo Text::_('COM_ISHOP_CART_MINUS'); ?>"
+                                            type="button"
+                                            data-cart-button="minus"><span class="visually-hidden"><?php echo Text::_('COM_ISHOP_CART_MINUS'); ?></span>-</button>
+                                    <input class="quantity_input"
+                                           id="quantity_input_<?php echo $key_id; ?>"
+                                           type="number"
+                                           name="quantity[<?php echo $product->id; ?>]"
+                                           value="<?php echo $product->incart_count; ?>"
+                                           min="0"
+                                           readonly
+                                           data-quantity>
+                                    <button class="btn btn-primary"
+                                            title="<?php echo Text::_('COM_ISHOP_CART_PLUS'); ?>"
+                                            type="button"
+                                            data-cart-button="plus"><span class="visually-hidden"><?php echo Text::_('COM_ISHOP_CART_PLUS'); ?></span>+</button>
+                                <?php endif; ?>
+                            </div>
                             <button class="btn btn-light"
                                     title="<?php echo Text::_('COM_ISHOP_CART_REMOVE'); ?>"
                                     type="button"
                                     data-cart-button="delete">
-                                <span class="visually-hidden"><?php echo Text::_('COM_ISHOP_CART_REMOVE'); ?></span>
                                 <?php echo LayoutHelper::render('itheme.icon', ['icon' => 'i-trash']); ?>
+                                <span class="d-none d-lg-inline"><?php echo Text::_('COM_ISHOP_CART_REMOVE'); ?></span>
                             </button>
                         </div>
                     </div>
@@ -140,35 +127,36 @@ if ($this->cart->total > 0) {
         </div>
         <div>
             <div class="module-cart-checkout">
-                <div class="module-cart-info">
-                    <h3><?php echo Text::_('COM_ISHOP_CART_YOUR_ORDER'); ?></h3>
-                    <div class="info-line">
-                        <span><?php echo Text::_('COM_ISHOP_CART_SELECTED_TOTAL'); ?>:</span>
-                        <span class="cart-price"><span data-cart-total><?php echo PriceHelper::renderPrice($this->cart->total, false); ?></span><span class="currency"><?php echo $currency; ?></span></span>
-                    </div>
-                    <div class="info-line">
-                        <span><?php echo Text::_('COM_ISHOP_CART_SELECTED_SALE'); ?>:</span>
-                        <span class="cart-price">-<span data-cart-total-discount><?php echo PriceHelper::renderPrice($this->cart->total_discount, false); ?></span><span class="currency"><?php echo $currency; ?></span></span>
-                    </div>
-                    <hr>
-                    <div class="info-line h5">
-                        <span><?php echo Text::_('COM_ISHOP_CART_SUM'); ?>:</span>
-                        <span class="cart-price"><span data-cart-summary><?php echo PriceHelper::renderPrice($this->cart->summary, false); ?></span><span class="currency"><?php echo $currency; ?></span></span>
-                    </div>
+                <h3><?php echo Text::_('COM_ISHOP_CART_YOUR_ORDER'); ?></h3>
+                <div class="info-line">
+                    <span><?php echo Text::_('COM_ISHOP_CART_SELECTED_TOTAL'); ?>:</span>
+                    <?php echo LayoutHelper::render('itheme.product.price', [
+                            'price' => $this->cart->total, 'class' => 'cart-price', 'attribs' => 'data-cart-total', 'minus' => true]); ?>
+                </div>
+                <div class="info-line">
+                    <span><?php echo Text::_('COM_ISHOP_CART_SELECTED_SALE'); ?>:</span>
+                    <?php echo LayoutHelper::render('itheme.product.price', [
+                            'price' => $this->cart->total, 'class' => 'cart-price', 'attribs' => 'data-cart-total-discount', 'minus' => true]); ?>
+                </div>
+                <div class="info-line h5">
+                    <span><?php echo Text::_('COM_ISHOP_CART_SUM'); ?>:</span>
+                    <?php echo LayoutHelper::render('itheme.product.price', [
+                            'price' => $this->cart->total, 'class' => 'cart-price', 'attribs' => 'data-cart-summary', 'minus' => true]); ?>
                 </div>
                 <?php if ($this->cart->total > 0) : ?>
-                <div><button class="btn btn-primary btn-lg"
-                             title="<?php echo Text::_('COM_ISHOP_CART_CHECKOUT'); ?>"
-                             type="submit"><?php echo Text::_('COM_ISHOP_CART_CHECKOUT'); ?></button></div>
+                    <button class="btn btn-primary btn-lg"
+                            title="<?php echo Text::_('COM_ISHOP_CART_CHECKOUT'); ?>"
+                            type="submit">
+                        <span><?php echo Text::_('COM_ISHOP_CART_CHECKOUT'); ?></span><?php echo LayoutHelper::render('itheme.icon', ['icon' => 'i-chevron-right']); ?>
+                    </button>
                 <?php endif; ?>
             </div>
         </div>
         <?php echo HTMLHelper::_('form.token'); ?>
     </form>
-    <?php else : ?>
-        <div class="module-cart-empty">
-            <?php echo LayoutHelper::render('itheme.icon', ['icon' => 'i-cart', 'class' => 'mega primary']); ?>
-            <p><?php echo Text::_('COM_ISHOP_CART_NULL'); ?></p>
-        </div>
-    <?php endif; ?>
-</div>
+<?php else : ?>
+    <div class="module-cart-empty">
+        <?php echo LayoutHelper::render('itheme.icon', ['icon' => 'i-cart', 'class' => 'mega primary']); ?>
+        <p><?php echo Text::_('COM_ISHOP_CART_NULL'); ?></p>
+    </div>
+<?php endif; ?>
---
+++ html/layouts/itheme/product/price.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package    tpl_itheme
+ * @author     Pavel Lange <pavel@ilange.ru>
+ * @link       https://github.com/i-lange/tpl_itheme
+ * @copyright  (C) 2026 Pavel Lange <https://ilange.ru>
+ * @license    GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+
+extract($displayData);
+
+/** @var float $price Цена */
+/** @var object $params Параметры магазина */
+/** @var string $class Дополнительный класс */
+/** @var string $attribs Дополнительные атрибуты */
+
+// Если не передали параметры, заберем из компонента
+if (empty($params)) {
+    $params = ComponentHelper::getParams('com_ishop');
+}
+$class = htmlspecialchars((!empty($class) ? ' ' . $class : ''), ENT_QUOTES, 'UTF-8');
+$class = htmlspecialchars((!empty($attribs) ? ' ' . $attribs : ''), ENT_QUOTES, 'UTF-8');
+$currency = $params->get('defaultCurrency', 'BYN');
+$icon = LayoutHelper::render('itheme.icon', ['icon' => 'i-' . $currency]);
+$round = (int) $params->get('roundPrice', 0);
+
+if ($price) : ?>
+<div class="<?php echo $class; ?>">
+    <?php echo (!empty($minus) && $minus === true) ? '-' : ''; ?><span<?php echo $attribs; ?>><?php echo round($price, $round); ?></span><span class="currency" aria-label="<?php echo $currency; ?>"><?php echo $icon; ?></span>
+</div>
+<?php endif;
